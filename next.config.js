90f40f72c0a5bc7eac6ab274c15d9c5089d88fa5
Yashveer Bika <yashveerbika@gmail.com>
2023-02-01 10:56:03 -0600

    remove leading / in assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  assetPrefix: isProd ? '/yashveer-bika.github.io/' : '',
+  assetPrefix: isProd ? 'yashveer-bika.github.io/' : '',
   images: {
     unoptimized: true,
   },
