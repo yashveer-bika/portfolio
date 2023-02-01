90e2fc34d8a392dae21791ffb2844999614fb505
Yashveer Bika <yashveerbika@gmail.com>
2023-02-01 03:19:37 -0600

    change site title to Yashveer Bika

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = 'Yashveer Bika';
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = 'Yashveer Bika';
 
 export default function Layout({ children, home }) {
   return (
